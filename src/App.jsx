820e0a73b67040e828eb6df901b6827df331536a
Chandra Bhushan Sah <123019803+Chandus2003@users.noreply.github.com>
2025-02-13 05:54:08 +0530

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 
-import Pokedex from './components/Pokedex/pokedex';
-import PokemonDet from './components/PokemonDet/pokemonDet'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import Pokedex from './components/Pokedex/pokedex.jsx';  // or '.js' if the file extension is .js
+import PokemonDet from './components/PokemonDet/pokemonDet.jsx';  // or '.js' if the file extension is .js
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
 
